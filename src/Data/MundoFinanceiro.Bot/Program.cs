fcc7e61969d7711d323a50314eaa9d5aef50df67
Daniel Cunha <danielcunha54@gmail.com>
2019-10-22 21:22:09 -0300

    Criado os projetos

---
+++ src/Data/MundoFinanceiro.Bot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MundoFinanceiro.Bot
+{
+    public static class Program
+    {
+        internal static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ src/Data/MundoFinanceiro.Database/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MundoFinanceiro.Database
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Console Application for Entity Framework Core migrations");
+        }
+    }
+}
