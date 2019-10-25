970fda26efc6c123ab0b18b5acb77473deaaa39b
Daniel Cunha <danielcunha54@gmail.com>
2019-10-24 22:05:27 -0300

    Criado primeira leva de papéis

---
+++ src/Data/MundoFinanceiro.Database/Migrations/20191025005539_CriadoPapeisIniciais.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MundoFinanceiro.Database.Migrations
+{
+    public partial class CriadoPapeisIniciais : Migration
+    {
+        private static readonly string[] PapeisInicias = new string[]
+        {
+            "PETR4",
+            "MGLU3",
+            "ELPL3",
+            "PRIO3",
+            "GGBR3",
+            "KLBN3",
+            "LAME3",
+            "BBAS3",
+            "ELET3",
+            "VVAR3"
+        };
+        
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            const string insertFormat = "INSERT INTO papeis (id, nome) VALUES({0}, '{1}')";
+
+            for (var index = 0; index < PapeisInicias.Length; index++)
+                migrationBuilder.Sql(string.Format(insertFormat, index + 1, PapeisInicias[index]));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
