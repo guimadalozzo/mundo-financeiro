33909745c94c6a68b35f4e04107a5f9d83076e42
lucascocco <lucas.exiled@hotmail.com>
2019-10-29 19:14:40 -0300

    api example

---
+++ playground/google-news/newsexample.js
@@ -0,0 +1,13 @@
+function BuscaNoticia(keyWord) {
+    var request = new XMLHttpRequest();
+
+// Open a new connection, using the GET request on the URL endpoint
+    request.open('GET', 'https://newsapi.org/v2/everything?q='+keyWord+'&apiKey=2aaa77be14d6496885ce7bb053b920cd', true);
+
+    request.onload = function() {
+        var data = this.response;
+        data = JSON.parse(data);
+        console.log(data)
+    };
+    request.send()
+}
